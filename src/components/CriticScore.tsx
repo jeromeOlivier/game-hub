ca919d18e0837a63193386a269b811b37d346343
jeromeOlivier <jerome.olivier@icloud.com>
2023-03-28 08:16:55 -0400

    added emojis currently commented out in game card component

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 
 const CriticScore = ({ score }: Props) => {
-  let color = score >= 90 ? "green" : score >= 60 ? "yellow" : "red";
+  let color = score >= 90 ? "green" : score >= 80 ? "yellow" : "red";
   return (
     <Badge colorScheme={color} fontSize="14px" padding={2} borderRadius="50%">
       {score}
